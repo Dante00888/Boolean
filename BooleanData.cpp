3900d33af3fbbf80f500cdb178da593e152baed0
CHRISTIAN DAYO <dantechristian783@gmail.com>
2024-07-18 11:58:53 +0300

    Add files via upload

---
+++ BooleanData.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    bool isCodingFun = true;
+    if(isCodingFun)
+    {
+        cout<<"C++ Programming is fun"<<endl;
+    }
+    else
+    {
+        cout<<"Coding is not fun!"<<endl;
+    }
+    return 0;
+}
